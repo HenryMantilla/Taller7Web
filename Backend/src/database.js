ce641a1ceeb6daac084410c3af970b0ac87b5ef8
HenryMantilla <dario30agosto@gmail.com>
2022-02-26 15:09:55 -0500

    Taller7 Web

--- Backend/src/database.js
+++ Backend/src/database.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb+srv://MongoDBtest:BD2.L1@cluster0.jfspj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
+mongoose.connect('mongodb+srv://MongoDBtest:BD2.L1@cluster0.jfspj.mongodb.net/Taller7?retryWrites=true&w=majority')
     .then(db => console.log('DB succesfully connected'))
     .catch((err) => console.error(err));
--- Backend/src/models/usuario_models.js
+++ Backend/src/models/usuario_models.js
@@ -2,7 +2,7 @@ const{Schema, model} = require('mongoose')
 
 const userDocument = new Schema({
     nombre: {type: String, required: true},
-    descripcion: {type: String, requiered: false}
+    descripcion: {type: String, requiered: true}
 },{
     versionKey: false
 })
